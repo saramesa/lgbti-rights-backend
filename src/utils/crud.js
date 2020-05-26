49399a5f92a21f5b6417a281b6c9a4e8e0b97c03
Sara Mesa <saramesaguerra@gmail.com>
2020-05-26 21:16:41 +0200

    fix response in getOne. Deleted unuseful array

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -15,8 +15,7 @@ export const getOne = model => async (req, res) => {
       return res.status(404).send({ error: 'No data found' })
     }
     const document = formatDocument([doc], req.baseUrl, lan)
-
-    res.status(200).json({ data: document })
+    res.status(200).json(document)
   } catch (e) {
     console.error(e)
     res.status(400).end()
